6bb389d2ba9a415266c629197280815af60b2efc
Wei-Zou <wz.weizou@gmail.com>
2019-07-05 20:15:47 -0700

    setup mock for kafkajs and test for mock passes

---
+++ __mocks__/kafkajs.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const kafkajs = jest.genMockFromModule('kafkajs');
+
+class Producer {
+  constructor ({ sendCb }) {
+    this.sendCb = sendCb;
+  }
+
+  async connect () {
+    return Promise.resolve();
+  }
+
+  async send ({ topic, messages }) {
+    this.sendCb({ topic, messages })
+  }
+
+  async disconnect () {
+    return Promise.resolve();
+  }
+}
+
+class Consumer {
+  constructor ({ groupId, subscribeCb }) {
+    this.groupId = groupId;
+    this.subscribeCb = subscribeCb;
+  }
+
+  getGroupId () {
+    return this.groupId;
+  }
+
+  async connect () {
+    return Promise.resolve();
+  }
+
+  async subscribe ({ topic }) {
+    this.subscribeCb(topic, this);
+  }
+
+  async run ({ eachMessage }) {
+    this.eachMessage = eachMessage;
+  }
+
+  async disconnect () {
+    return Promise.resolve();
+  }
+}
+
+kafkajs.Kafka = class Kafka {
+  constructor (config) {
+    this.brokers = config.brokers;
+    this.clientId = config.clientId;
+    this.topics = {};
+  }
+
+  _subscribeCb (topic, consumer) {
+    this.topics[topic] = this.topics[topic] || {};
+    const topicObj = this.topics[topic];
+    topicObj[consumer.getGroupId()] = topicObj[consumer.getGroupId()] || [];
+    topicObj[consumer.getGroupId()].push(consumer);
+  }
+
+  _sendCb ({ topic, messages }) {
+    messages.forEach((message) => {
+      Object.values(this.topics[topic]).forEach((consumers) => {
+        consumers[0].eachMessage({ // only send to the first consumer for now
+          message,
+        });
+      });
+    });
+  }
+
+  producer () {
+    return new Producer({
+      sendCb: this._sendCb.bind(this),
+    });
+  }
+
+  consumer ({ groupId }) {
+    return new Consumer({
+      groupId,
+      subscribeCb: this._subscribeCb.bind(this),
+    });
+  }
+};
+
+module.exports = kafkajs;
