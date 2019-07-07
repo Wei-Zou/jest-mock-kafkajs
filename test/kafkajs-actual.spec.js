bab761981544573c75cdc868ceeedcd6c803682d
Wei-Zou <wz.weizou@gmail.com>
2019-07-06 21:35:19 -0700

    test multiple consumers in the same group share the same set of messages

--- test/kafkajs-actual.spec.js
+++ test/kafkajs-actual.spec.js
@@ -23,8 +23,8 @@ const runConsumer = async ({ existingConsumer, kafkaConnection, groupId, topic, 
 
 describe('test kafkajs with actual Kafka instance', () => {
   it('should produce and consume messages successfully', async (done) => {
-    const topic = 'test-actual-kafka-instance1';
-    const groupId = 'test-actual-kafka-instance-group';
+    const topic = 'test-kafkajs-actual-1';
+    const groupId = 'test-kafkajs-actual-group1';
     const kafkaConnection = new Kafka(initConfig);
 
     const producer = kafkaConnection.producer();
@@ -63,5 +63,148 @@ describe('test kafkajs with actual Kafka instance', () => {
     ];
 
     await sendMessages({ producer, topic, messages });
-  }, 5000);
+  }, 20000);
+
+  test('consumers who subscribe to the same topic and in the same group share one set of messages', async (done) => {
+    const topic = 'test-kafkajs-actual-2';
+    const groupId = 'test-kafkajs-actual-group';
+    const kafkaConnection = new Kafka(initConfig);
+
+    const producer = kafkaConnection.producer();
+
+    const messagesConsumed1 = {};
+    const messagesConsumed2 = {};
+    let consumer1;
+    let consumer2;
+
+    const getEachMessageCb = ({ collectedMessages, otherMessages }) => {
+      return async ({ message }) => {
+        console.log(
+          `consuming message key=${message.key.toString()}, value=${message.value.toString()}`
+        );
+        collectedMessages[message.key.toString()] = message.value.toString();
+        console.log(
+          `collectedMessages=${JSON.stringify(collectedMessages)}, otherMessages=${JSON.stringify(
+            otherMessages
+          )}`
+        );
+
+        if (Object.keys(collectedMessages).length + Object.keys(otherMessages).length >= 3) {
+          expect({ ...collectedMessages, ...otherMessages }).toEqual({
+            key1: 'value1',
+            key2: 'value2',
+            key3: 'value3',
+          });
+          await consumer1.disconnect();
+          await consumer2.disconnect();
+          await producer.disconnect();
+          done();
+        }
+      };
+    };
+
+    consumer1 = await runConsumer({
+      kafkaConnection,
+      groupId,
+      topic,
+      eachMessage: getEachMessageCb({
+        collectedMessages: messagesConsumed1,
+        otherMessages: messagesConsumed2,
+      }),
+    });
+    consumer2 = await runConsumer({
+      kafkaConnection,
+      groupId,
+      topic,
+      eachMessage: getEachMessageCb({
+        collectedMessages: messagesConsumed2,
+        otherMessages: messagesConsumed1,
+      }),
+    });
+
+    const messages = [
+      {
+        key: 'key1',
+        value: 'value1',
+      },
+      {
+        key: 'key2',
+        value: 'value2',
+      },
+      {
+        key: 'key3',
+        value: 'value3',
+      },
+    ];
+
+    await sendMessages({ producer, topic, messages });
+  }, 20000);
+
+  test('consumers who subscribe to the same topic but in different group get a complete set of messages', async (done) => {
+    const topic = 'test-kafkajs-actual-3';
+    const groupId = 'test-kafkajs-actual-group';
+    const kafkaConnection = new Kafka(initConfig);
+
+    const producer = kafkaConnection.producer();
+
+    const messagesConsumed1 = {};
+    const messagesConsumed2 = {};
+    let consumer1;
+    let consumer2;
+
+    const getEachMessageCb = ({ collectedMessages, otherMessages }) => {
+      return async ({ message }) => {
+        collectedMessages[message.key.toString()] = message.value.toString();
+
+        if (Object.keys(collectedMessages).length >= 3 && Object.keys(otherMessages).length >= 3) {
+          expect(collectedMessages).toEqual({
+            key1: 'value1',
+            key2: 'value2',
+            key3: 'value3',
+          });
+          expect(otherMessages).toEqual(collectedMessages);
+          await consumer1.disconnect();
+          await consumer2.disconnect();
+          await producer.disconnect();
+          done();
+        }
+      };
+    };
+
+    consumer1 = await runConsumer({
+      kafkaConnection,
+      groupId: `${groupId}-1`,
+      topic,
+      eachMessage: getEachMessageCb({
+        collectedMessages: messagesConsumed1,
+        otherMessages: messagesConsumed2,
+      }),
+    });
+    consumer2 = await runConsumer({
+      kafkaConnection,
+      groupId: `${groupId}-2`,
+      topic,
+      eachMessage: getEachMessageCb({
+        collectedMessages: messagesConsumed2,
+        otherMessages: messagesConsumed1,
+      }),
+    });
+
+    const messages = [
+      {
+        key: 'key1',
+        value: 'value1',
+      },
+      {
+        key: 'key2',
+        value: 'value2',
+      },
+      {
+        key: 'key3',
+        value: 'value3',
+      },
+    ];
+
+    await sendMessages({ producer, topic, messages });
+  }, 20000);
 });
